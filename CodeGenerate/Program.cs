4bebc6d2a2d0e48fd62441332b339ed9acf2680e
Fatih Emre Sarman <Fatih Emre Sarman@DESKTOP-R04PVQ3>
2023-10-10 17:47:51 +0300

    Add project files.

---
+++ CodeGenerate/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            byte[] keyBytes = new byte[32]; // 256 bit = 32 byte
+            rng.GetBytes(keyBytes);
+
+            string keyHex = BitConverter.ToString(keyBytes).Replace("-", "").ToLower();
+
+            Console.WriteLine("256 bitlik rastgele anahtar (hex formatında):");
+            Console.WriteLine(keyHex);
+        }
+    }
+}
